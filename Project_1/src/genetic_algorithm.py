dad26720931ff13ef45f1e8ccbef79d32d2eef59
Lasse <lassehhenriksen@gmail.com>
2018-01-30 15:05:35 +0100

    Refactoring

--- Project_1/src/genetic_algorithm.py
+++ Project_1/src/genetic_algorithm.py
@@ -4,8 +4,8 @@ import math
 
 class GA:
     def __init__(self, fileName, population_size):
-        self.problem_object = pr.ProblemSpec()
-        self.problem_object.readProblemFile(fileName)
+        self.problem_spec = pr.ProblemSpec()
+        self.problem_spec.readProblemFile(fileName)
         self.population_size = population_size
 
         self.population = []
@@ -13,7 +13,7 @@ class GA:
 
     def initializePopulation(self):
         for _ in range(0, self.population_size):
-            genotype = Genotype(self.problem_object)
+            genotype = Genotype(self.problem_spec)
             self.population.append(genotype)
         pass
 
@@ -33,24 +33,25 @@ class GA:
         pass
 
 class Genotype:
-    def __init__(self, problem_object):
-        max_vehicles = problem_object.max_vehicles_per_depot * problem_object.nr_depots
+    def __init__(self, problem_spec):
+        max_vehicles = problem_spec.max_vehicles_per_depot * problem_spec.nr_depots
         self.vehicle_routes = [ [] for i in range(0, max_vehicles)]
         random.seed()
-        for customer in problem_object.customers:
+        for customer in problem_spec.customers:
             inserted = False
             while(not inserted):
-                vehicle = random.randrange(max_vehicles)  # Choose random vehicle
-                if not self.vehicleOverloaded(self.vehicle_routes[vehicle], vehicle, customer.q, problem_object): # Only append if it doesnt cause vehicle overload
-                    self.vehicle_routes[vehicle].append(customer)
+                vehicle_nr = random.randrange(max_vehicles)  # Choose random vehicle
+                # Only append if it doesnt cause vehicle overload :
+                if not self.vehicleOverloaded(self.vehicle_routes[vehicle_nr], vehicle_nr, customer.q, problem_spec):
+                    self.vehicle_routes[vehicle_nr].append(customer)
                     inserted = True
 
-    def vehicleOverloaded(self, vehicle_route, vehicle, customer_demand, problem_object):
+    def vehicleOverloaded(self, vehicle_route, vehicle_nr, customer_demand, problem_spec):
         demand_sum = customer_demand
         for customer in vehicle_route:
             demand_sum += customer.q
-        depot_number = math.floor(vehicle / problem_object.nr_depots)
-        if demand_sum > problem_object.depots[depot_number - 1].Q:
+        depot_number = math.floor(vehicle_nr / problem_spec.nr_depots)
+        if demand_sum > problem_spec.depots[depot_number - 1].Q:
             return True
         else:
             return False
