8487177d15f7a86f3d3e7dc71eeed4fb9e595b9c
Lasse <lassehhenriksen@gmail.com>
2018-01-31 21:13:46 +0100

    Implement the rest of the evolution cycle
    + elite function
    + tournament selection
    + better init of genotypes

--- Project_1/src/genetic_algorithm.py
+++ Project_1/src/genetic_algorithm.py
@@ -3,7 +3,7 @@ import random
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+from timeit import default_timer as timer
 
 
 colors = ['crimson', 'green', 'blue',
@@ -12,51 +12,88 @@ colors = ['crimson', 'green', 'blue',
         'greenyellow', 'grey', 'aqua', 'olive',  'teal']
 
 class GA:
-    def __init__(self, fileName, population_size, generations, crossover_prob, intra_mutation_prob, inter_mutation_prob):
+    def __init__(self, fileName, population_size, generations, elite_ratio, tournament_ratio, crossover_prob, intra_mutation_prob, inter_mutation_prob):
         self.problem_spec = pr.ProblemSpec(fileName)
 
         # Parameters
         self.population_size = population_size
         self.generations = generations
+        self.elite_ratio = elite_ratio
+        self.tournament_ratio = tournament_ratio
         self.crossover_prob = crossover_prob
         self.intra_mutation_prob = intra_mutation_prob
         self.inter_mutation_prob = inter_mutation_prob
 
         # Data storage
         self.population = []
-        self.fitness_history = []
+        self.average_fitness_history = []
+        self.best_fitness_history = []
 
     ######################################################
     # Create an initial population by creating Genotype()'s and storing them in self.population
     def initializePopulation(self):
+        start = timer()
         for _ in range(0, self.population_size):
             genotype = Genotype(self.problem_spec)
             self.population.append(genotype)
+        end = timer()
+        print("initalizePopulation timer: " + str(end-start))
 
 
-    ######################################################
-    # Returns the fitness (float) of a single individual
-    def fitness(self, individual):
-        return 0
-
-    def parentSelection(self):
+    def intraMutation(self, offspring):
         pass
 
-    def mutation(self, parent):
+    def interMutation(self, offspring):
         pass
 
     def crossover(self, parent1, parent2):
         pass
 
-    def survivorSelection(self):
-        pass
+    ######################################################
+    # Returns the parent with the highest fitness from the parents parameter
+    def getBestParent(self, parents):
+        fitnesses = []
+        for parent in parents:
+            fitnesses.append(parent.fitness)
+        return parents[fitnesses.index(max(fitnesses))]
+
+
+    ######################################################
+    # Return the top self.elites percent of the poplation
+    def getPopulationElite(self):
+        population = list(self.population)
+        fitnesses = []
+        for individual in population:
+            fitnesses.append(individual.fitness)
+        sorted_population = [x for _, x in sorted(zip(fitnesses, population))] #sorts population in descending order based on fitnesses
+        elites_num = int(math.ceil(self.population_size * self.elite_ratio))
+        elites = sorted_population[0:elites_num]
+        return elites
+
+    ######################################################
+    # Return two genotypes from the population based on a tournament selection
+    def parentSelection(self):
+        num_individuals = int(math.ceil(self.tournament_ratio * self.population_size))
+        if num_individuals < 2:
+            num_individuals = 2
+
+        selected = random.sample(range(0, self.population_size), num_individuals)
+        tournament_population = []
+        for i in selected:
+            tournament_population.append(self.population[i])
+        fitnesses = []
+        for i in tournament_population:
+            fitnesses.append(i.fitness)
+        sorted_tournament_population = [x for _, x in sorted(zip(fitnesses, tournament_population))]  # sorts population in descending order based on fitnesses
+
+        return sorted_tournament_population[0:2]
 
     ######################################################
     # Evolves an initial population by means of mutation and recombinations over a given number of generations
     def evolutionCycle(self):
         random.seed(None)
 
-        # Generate initial population
+        # Generate the initial population
         self.initializePopulation()
 
         # Start the evolution process
@@ -64,17 +101,50 @@ class GA:
             # Evaluate the fitness of all individuals in the population and compute the average
             population_fitness = []
             for individual in self.population:
-                population_fitness.append(self.fitness(individual))
+                population_fitness.append(individual.fitness)
+
+            # Store fitness data
             average_fitness = sum(population_fitness) / self.population_size
-            self.fitness_history.append(average_fitness)
+            best_fitness = min(population_fitness)
+            self.average_fitness_history.append(average_fitness)
+            self.best_fitness_history.append(best_fitness)
 
-            # Create a new population with 200 individuals by doing recombination, mutation and elitism
+            # Print generation data to terminal
+            print("Generation: %d" % generation)
+            print("Best fitness score: %f\n" % best_fitness)
+
+            # Create a new population and directly copy the elites of the previous generation to the new one
             new_population = []
-            while len(new_population) < len(self.population_size):
+            elites = self.getPopulationElite()
+            new_population += elites
+
+            # Populate the new population until it has 200 individuals by doing recombination, mutation and survivors
+            while len(new_population) < (self.population_size):
+                # Do tournament selection to choose parents
+                parents = self.parentSelection()
+
+                # Roll dice on what event shall happen (recombination or survivors)
+                event = random.random()
+                if event < self.crossover_prob:
+                    # Do crossover and pass offspring to next generation
+                    offspring = self.crossover(parents[0], parents[1])
+
+                else:
+                    offspring = self.getBestParent(parents)
+
+                # Roll dice on what event shall happen (inter-mutation, intra-mutation or no mutation)
+                event = random.random()
+                if event < self.inter_mutation_prob:
+                    offspring = self.interMutation(offspring)
+                elif (self.inter_mutation_prob < event and event < self.inter_mutation_prob + self.intra_mutation_prob):
+                    offspring = self.intraMutation(offspring)
+                new_population += offspring
 
 
 
-                pass
+
+
+
 
 
 
@@ -96,13 +166,39 @@ class Genotype:
         self.problem_spec = problem_spec
         random.seed()
         for customer in problem_spec.customers:
+            # Choose depot based on tournament selection
+            random_depots = random.sample(range(0, problem_spec.num_depots), int(math.ceil(problem_spec.num_depots * 0.60)))
+            closest_depot_distance = float('Inf')
+            closest_depot = random_depots[0]
+            for depot_num in random_depots:
+                if problem_spec.cost_matrix[customer.i - 1, depot_num] < closest_depot_distance:
+                    closest_depot_distance = problem_spec.cost_matrix[customer.i - 1, depot_num]
+                    closest_depot = depot_num
+
             inserted = False
-            while(not inserted):
-                vehicle_nr = random.randrange(max_vehicles)  # Choose random vehicle
-                # Only append if it doesnt cause vehicle overload :
-                if not self.vehicleOverloaded(self.vehicle_routes[vehicle_nr], vehicle_nr, customer.q, problem_spec):
-                    self.vehicle_routes[vehicle_nr].append(customer)
-                    inserted = True
+            # Attempt to put the customer in the next depot if the selected one fails
+            for depot in range(closest_depot, closest_depot + problem_spec.num_depots):
+                depot = depot % problem_spec.num_depots
+                vehicle_start_index = depot * problem_spec.max_vehicles_per_depot
+
+                # Proceed by putting the customer in the first available vehicle
+                for vehicle_nr in range(vehicle_start_index, vehicle_start_index + problem_spec.max_vehicles_per_depot - 1):
+                    # Only append if it doesnt cause vehicle overload :
+                    if not self.vehicleOverloaded(self.vehicle_routes[vehicle_nr], vehicle_nr, customer.q, problem_spec):
+                        self.vehicle_routes[vehicle_nr].append(customer)
+                        inserted = True
+                        break
+                if inserted:
+                    break
+
+
+        self.fitness = self.duration()
+
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
+    def __gt__(self, other):
+        return self.fitness > other.fitness
 
     ######################################################
     # Determine if the a vehicle becomes overloaded if its assigned an additional customer, returns True/False
@@ -111,7 +207,7 @@ class Genotype:
         for customer in vehicle_route:
             demand_sum += customer.q
         depot_number = self.getDepotNumber(vehicle_nr)
-        if demand_sum > problem_spec.depots[depot_number - 1].Q:
+        if demand_sum > problem_spec.depots[depot_number].Q:
             return True
         else:
             return False
@@ -172,9 +268,15 @@ class Genotype:
 
 
 
-ga = GA('p01', 10, 100, 0.6, 0.2, 0.25)
-ga.initializePopulation()
-dur = ga.population[0].duration()
+ga = GA(fileName = 'p01', population_size = 500, generations = 1000,
+        elite_ratio = 0.02, tournament_ratio = 0.15,
+        crossover_prob = 0.6, intra_mutation_prob = 0.2, inter_mutation_prob = 0.25)
+
+
+ga.evolutionCycle()
+#ga.initializePopulation()
+#dur = ga.population[0].duration()
+
 #ga.population[0].vizualizeGenes()
 #ga.population[1].vizualizeGenes()
 #plt.close("all")
