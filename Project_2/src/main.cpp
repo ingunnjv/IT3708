912f6416cb3237ebcf868abc0ac88e881c61c7fe
ingunnjv <ingunn.vallestad@gmail.com>
2018-02-23 11:14:47 +0100

    Made OpenCV work

--- Project_2/src/main.cpp
+++ Project_2/src/main.cpp
@@ -3,14 +3,23 @@
 #include <vector>
 #include <Eigen/Dense>
 #include "image_loader.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 using namespace std;
 using Eigen::MatrixXd;
 
 int main() {
     cout << "Hello, World!" << endl;
 
+    cv::Mat image;
+    image = cv::imread("../Test Images/1/s6.jpg", cv::IMREAD_COLOR);
+    cv::imshow("Displayed image", image);
+    cv::waitKey(0);
+
     ImageLoader test = ImageLoader();
-    cout << test.B;
+    cout << test.B << endl;
 
     MatrixXd m(2,2);
     m(0,0) = 3;
