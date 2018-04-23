99657f9a9c6449fee2a806e66250a9d67c46b2e9
Lasse <lassehhenriksen@gmail.com>
2018-04-23 14:29:34 +0200

    Update best bee pointer

--- Project_3/src/abc.h
+++ Project_3/src/abc.h
@@ -23,9 +23,7 @@ private:
     JSSP* jssp;
     std::vector<bee> employed_bees;
     std::vector<int> old_bees_indices;
-    bee* idiet_loser_bee;
     bee* super_amazing_bee;
-    // maybe save abandoned_bees?
 
     int num_food_sources;
     int abandonment_limit;
@@ -35,7 +33,6 @@ private:
     std::vector<int> neighbour_list;
     std::vector<int> winning_neighbour_list;
     int nl_length;
-
 
 public:
     ABC(JSSP &jssp, int num_food_sources, int abandonment_limit, int cycles, int nl_length, double p_local_search);
